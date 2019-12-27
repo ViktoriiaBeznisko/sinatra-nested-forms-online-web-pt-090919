115be00a55243bc80f71fd345d66cf744907a3f8
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-27 14:35:18 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,17 @@
 class Ship
-
-ame, type, and booty
-
+  
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
